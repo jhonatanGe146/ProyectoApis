3162dee66d1d17150b31b73638368ceb5ee52d0c
JhonatanGel146 <115653316+jhonatanGe146@users.noreply.github.com>
2024-03-08 19:16:59 -0500

    Update models.py de huesped

--- mod_guest/models.py
+++ mod_guest/models.py
@@ -4,12 +4,11 @@ from django.utils import timezone
 default_value = timezone.now()
 
 class huesped(models.Model):
-    IDHUESPED =  models.AutoField(primary_key=True, null=False)
-    NRODOCUMENTO = models.CharField(max_length=10)
-    NOMBRE = models.CharField(max_length=70)
-    APELLIDO = models.CharField(max_length=70)
+    NRODOCUMENTO = models.CharField(primary_key=True, null=False, max_length=10)
+    NOMBRE = models.CharField(max_length=30)
+    APELLIDO = models.CharField(max_length=30)
     EMAIL = models.EmailField(unique=True)
-    TELEFONO = models.CharField(max_length=15)
+    TELEFONO = models.CharField(max_length=12)
 
     def __str__(self):
         return (f'{self.NRODOCUMENTO} || {self.NOMBRE} || {self.APELLIDO}  ')
